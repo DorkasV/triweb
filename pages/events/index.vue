cd683d79b9a00001dd96ab80a9ab6ef964c6dc55
DorkasV <neturiu.kaipgaila@gmail.com>
2019-12-17 14:30:22 +0200

    remove pagination info

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -21,7 +21,6 @@
     >
     </vuetable>
     <div class="vuetable-pagination ui basic segment grid">
-      <vuetable-pagination-info ref="paginationInfo"></vuetable-pagination-info>
       <vuetable-pagination ref="pagination"
         :css="css.pagination"
         @vuetable-pagination:change-page="onChangePage">
@@ -81,7 +80,6 @@ export default {
     },
     onPaginationData(paginationData) {
       this.$refs.pagination.setPaginationData(paginationData)
-      this.$refs.paginationInfo.setPaginationData(paginationData)
     },
     onChangePage(page) {
       this.$refs.events.changePage(page)
